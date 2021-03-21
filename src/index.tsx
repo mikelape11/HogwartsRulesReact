b586b9fe682df4c1688245765b83f13ccf864046
enetz7 <enetz7@outlook.com>
2021-03-21 22:25:20 +0100

    Login

--- src/index.tsx
+++ src/index.tsx
@@ -1,18 +1,21 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import reportWebVitals from './reportWebVitals';
-import Demo from './views/components/login/index';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import reportWebVitals from "./reportWebVitals";
+import Demo from "./views/components/login/index";
+import { BrowserRouter, Switch, Route } from "react-router-dom";
+import { Redirect } from "react-router";
 
 ReactDOM.render(
   <React.StrictMode>
-    <Demo></Demo>
+    <BrowserRouter>
+      <Switch>
+        <Redirect exact from="/" to="/login" />
+        <Route path="/login" component={Demo} />
+        <Route path="/home" />
+      </Switch>
+    </BrowserRouter>
   </React.StrictMode>,
-  document.getElementById('root')
+  document.getElementById("root")
 );
-
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
--- src/views/components/login/index.tsx
+++ src/views/components/login/index.tsx
@@ -1,62 +1,87 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import { Form, Input, Button, Checkbox } from 'antd';
-import './style/style.css';
-import 'antd/dist/antd.css';
-import Logo3 from './media/Logo3.png';
-
+import React, { useState } from "react";
+import { useHistory } from "react-router";
+import ReactDOM from "react-dom";
+import { Form, Input, Button, Checkbox } from "antd";
+import "./style/style.css";
+import "antd/dist/antd.css";
+import Logo3 from "./media/Logo3.png";
+import axios from "axios";
+//funcion que conecte con la api
 const layout = {
-    labelCol: { span: 8 },
-    wrapperCol: { span: 16 },
+  labelCol: { span: 8 },
+  wrapperCol: { span: 16 },
+};
+const tailLayout = {
+  wrapperCol: { offset: 8, span: 16 },
+};
+
+const Demo = () => {
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+  let history = useHistory();
+  function comprobarLogin() {
+    axios({
+      method: "POST",
+      url: "http://localhost:8081/login",
+      data: {
+        usuario: username,
+        password: password,
+      },
+      headers: { "Access-Control-Allow-Origin": "*" },
+    }).then((response: any) => {
+      console.log(response.data);
+      if (response.data == true) {
+        history.push("/home");
+      }
+    });
+  }
+  const onFinish = (values: any) => {
+    console.log("Success:", values);
   };
-  const tailLayout = {
-    wrapperCol: { offset: 8, span: 16 },
+
+  const onFinishFailed = (errorInfo: any) => {
+    console.log("Failed:", errorInfo);
   };
-  
-  const Demo = () => {
-    const onFinish = (values: any) => {
-      console.log('Success:', values);
-    };
-  
-    const onFinishFailed = (errorInfo: any) => {
-      console.log('Failed:', errorInfo);
-    };
-  
-    return (
-      <div className="logo"><img src={Logo3} alt="Girl in a jacket"></img></div>
-      /*<Form
-        {...layout}
-        name="basic"
-        initialValues={{ remember: true }}
-        onFinish={onFinish}
-        onFinishFailed={onFinishFailed}
+
+  return (
+    /*<div className="logo"><img src={Logo3} alt="Girl in a jacket"></img></div>*/
+    <Form
+      {...layout}
+      name="basic"
+      initialValues={{ remember: true }}
+      onFinish={onFinish}
+      onFinishFailed={onFinishFailed}
+    >
+      <Form.Item
+        label="Username"
+        name="username"
+        rules={[{ required: true, message: "Please input your username!" }]}
       >
-        <Form.Item
-          label="Username"
-          name="username"
-          rules={[{ required: true, message: 'Please input your username!' }]}
+        <Input onChange={(text) => setUsername(text.target.value)} />
+      </Form.Item>
+
+      <Form.Item
+        label="Password"
+        name="password"
+        rules={[{ required: true, message: "Please input your password!" }]}
+      >
+        <Input.Password onChange={(text) => setPassword(text.target.value)} />
+      </Form.Item>
+
+      <Form.Item {...tailLayout} name="remember" valuePropName="checked">
+        <Checkbox>Remember me</Checkbox>
+      </Form.Item>
+
+      <Form.Item {...tailLayout}>
+        <Button
+          type="primary"
+          htmlType="submit"
+          onClick={() => comprobarLogin()}
         >
-          <Input />
-        </Form.Item>
-  
-        <Form.Item
-          label="Password"
-          name="password"
-          rules={[{ required: true, message: 'Please input your password!' }]}
-        >
-          <Input.Password />
-        </Form.Item>
-  
-        <Form.Item {...tailLayout} name="remember" valuePropName="checked">
-          <Checkbox>Remember me</Checkbox>
-        </Form.Item>
-  
-        <Form.Item {...tailLayout}>
-          <Button type="primary" htmlType="submit">
-            Submit
-          </Button>
-        </Form.Item>
-      </Form>*/
-    );
-  };
-  export default Demo;
+          Submit
+        </Button>
+      </Form.Item>
+    </Form>
+  );
+};
+export default Demo;
